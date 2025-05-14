ef785da7041d03a4ea969ab0559e8ced495e7cb8
hoangnh4204 <hoangnh4204@gmail.com>
2025-05-14 21:53:24 +0700

    on tap viet doc api va phan trang

---
+++ OnTap2/src/test/java/com/hit/ontap2/OnTap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hit.ontap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
